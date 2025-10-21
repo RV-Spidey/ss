1cf854e4558934bb437317ddf42c4dda74d1b90b
RV-Spidey <killerkingmega10@gmail.com>
2025-10-21 06:48:16 +0530

    Update absolute.c

--- absolute/absolute.c
+++ absolute/absolute.c
@@ -15,37 +15,39 @@ for(i=2,j=0;i<7;i++,j++)
 name1[j]='\0';
 printf("name from object file %s\n",name1);
 if(strcmp(name,name1)==0)
-        {
+{
         do
-          {
-          fscanf(fp,"%s",line);
-          if(line[0]=='T')
+        {
+                fscanf(fp,"%s",line);
+                if(line[0]=='T')
                 {
-                for(i=2,j=0;i<8;i++,j++)
+                        for(i=2,j=0;i<8;i++,j++)
                          staddr[j]=line[i];
-                staddr[j]='\0';
-                staddr1=atoi(staddr);
-                i=12;
-                while(line[i]!='\0')
+                        staddr[j]='\0';
+                        staddr1=atoi(staddr);
+                        i=12;
+                        while(line[i]!='\0')
                         {
-                        if(line[i]!='^')
+                                if(line[i]!='^')
                                 {
-                                printf("00%d\t%c%c\n",staddr1,line[i],line[i+1]);
-                                staddr1++;
-                                i=i+2;
+                                        printf("00%d\t%c%c\n",staddr1,line[i],line[i+1]);
+                                        staddr1++;
+                                        i=i+2;
                                 }
-                        else
-                                i++;
+                                else
+                                        i++;
                         }
                 }
-        else if(line[0]=='E')
+                                
+                        
+                else if(line[0]=='E')
                 {
-                printf("\nExecution Address");
-                for(i=2;i<8;i++)
-                        printf("%c",line[i]);
-                break;
+                        printf("\nExecution Address");
+                        for(i=2;i<8;i++)
+                                printf("%c",line[i]);
+                        break;
                 }
-          }
+        }
         while(!feof(fp));
         }
 fclose(fp);
