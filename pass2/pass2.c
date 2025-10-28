633e78ce0bc59823eed3f13ca3907c39bc87f15f
RV-Spidey <killerkingmega10@gmail.com>
2025-10-28 17:58:49 +0530

    Update pass2.c

--- pass2/pass2.c
+++ pass2/pass2.c
@@ -1,85 +1,109 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
-int main() { 
-    char opcode[20], operand[20], symbol[20], label[20], code[20], mnemonic[25]; 
-    char add[20], objectcode[20]; 
-    int flag, flag1, locctr, loc; 
-    int startAddr = 0, progLen = 0; 
-    FILE *fp1, *fp2, *fp3, *fp4; 
-    fp1 = fopen("Out3.txt", "r"); 
-    fp2 = fopen("Twoout.txt", "w"); 
-    fp3 = fopen("Opcode.txt", "r"); 
-    fp4 = fopen("Sym1.txt", "r"); 
-    if (!fp1 || !fp2 || !fp3 || !fp4) { 
-        printf("Error: One or more files could not be opened.\n"); 
-        exit(1); 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+int main()
+{
+  int startaddress,address,finaladdress,length,oplen,s;
+  
+  char label[10],opcode[10], operand [10],mnemonic[10],code[10],symbol[10],symbaddr[10],ch;
+  
+  
+  printf("opening files\n");
+  FILE *intermediate,*objcode,*symtab,*optab,*output;
+  intermediate=fopen("intermediate.txt","r");     
+  objcode=fopen("objcode.txt","w");
+  output=fopen("output.txt","w");
+        if(intermediate==NULL||objcode==NULL||output==NULL)  //if error in opening files then shows error
+      printf("error in opening files\n");
+     
+  //reads a line from intermidiate
+  fscanf(intermediate,"%s%s%s",label,opcode,operand);  
+  if(strcmp(opcode,"START")==0)
+      startaddress=atoi(operand);     
+  
+//goes to last address for finding length 
+  while (strcmp(opcode,"END")!=0)
+        fscanf(intermediate,"%d%s%s%s",&address,label,opcode,operand);    
+  finaladdress=address;
+  length=finaladdress-startaddress;
+
+//close and reopen to start from begining 
+  fclose(intermediate);
+  intermediate=fopen("intermediate.txt","r");
+//read a line and is start the write the header record anf first line in objcode and intermedite
+  fscanf(intermediate,"%s%s%s",label,opcode,operand);
+  if(strcmp(opcode,"START")==0)
+  { 
+      fprintf(objcode,"H^%s^00%d^00%d\n",label,startaddress,finaladdress);
+      fprintf(output,"%s\t%s\t%s\n",label,opcode,operand);
+      fprintf(objcode,"T^00%d^%d",startaddress,length);
+  }
+  fscanf(intermediate,"%d%s%s%s",&address,label,opcode,operand);
+  
+ while (strcmp(opcode,"END")!=0) 
+  {       
+      if(strcmp(opcode,"BYTE")==0)
+      {
+          fprintf(output,"%d\t%s\t%s\t%s\t",address,label,opcode,operand );
+          oplen=strlen(operand)-3;  //if byte in form C'EOF' , so removing the count for C,','
+          fprintf(objcode,"^");
+          for(int i=2;i<(oplen+2);i++)
+          {
+              s=toascii(operand[i]); //convert to hex
+              fprintf(objcode,"%x",s); //eg E->45,0->4F,F->46 Then EOF to 454F46
+              fprintf(output,"%x",s);
+              
+          }
+          fprintf(output,"\n");
+      }
+     // if word print as it is with 0's for padding
+     else if(strcmp(opcode,"WORD")==0)
+      {
+          fprintf(output,"%d\t%s\t%s\t%s\t00000%s\n",address,label,opcode,operand,operand);
+          
+          fprintf(objcode,"^00000%s",operand);
+      }
+      //no machine code for resb,resw
+     else if(strcmp(opcode,"RESB")==0 || strcmp(opcode,"RESW")==0)
+      {
+          fprintf(output,"%d\t%s\t%s\t%s\n",address,label,opcode,operand );
+      }
+      else
+      {
+          symtab=fopen("symtab.txt","r");
+          optab=fopen("optab.txt","r");
+          while(fscanf(optab,"%s%s",mnemonic,code)==2)
+          {
+              if(strcmp(mnemonic,opcode)==0)
+              break;
+          }
+          
+          while(fscanf(symtab,"%s%s",symbol,symbaddr)==2)
+          {
+              if(strcmp(symbol,operand)==0)
+              { 
+              break;}
+          }
+          
+          fprintf(output,"%d\t%s\t%s\t%s\t%s%s\n",address,label,opcode,operand,code,symbaddr);
+          
+          fprintf(objcode,"^%s%s",code,symbaddr);
+          
+          fclose(symtab);
+          fclose(optab);
+      }
+      fscanf(intermediate,"%d%s%s%s",&address,label,opcode,operand);
+      
     } 
-    fscanf(fp1, "%s%s%s", label, opcode, operand); 
-    if (strcmp(opcode, "START") == 0) { 
-        startAddr = atoi(operand); 
-        fprintf(fp2, "%s\t%s\t%s\n", label, opcode, operand); 
-        fscanf(fp1, "%d%s%s%s", &locctr, label, opcode, operand); 
-    } 
-    char textRecord[500] = ""; 
-    int textStart = locctr; 
-    int textLen = 0; 
-    while (strcmp(opcode, "END") != 0) { 
-        flag = 0; 
-        rewind(fp3); 
-        while (fscanf(fp3, "%s%s", code, mnemonic) != EOF) { 
-            if (strcmp(opcode, code) == 0) { 
-                flag = 1; 
-                break; 
-            } 
-        } 
-        if (flag == 1) { 
-            flag1 = 0; 
-            rewind(fp4); 
-            while (fscanf(fp4, "%s%d", symbol, &loc) != EOF) { 
-                if (strcmp(symbol, operand) == 0) { 
-                    flag1 = 1; 
-                    break; 
-                } 
-            } 
-            if (flag1 == 1) { 
-                sprintf(add, "%04X", loc); 
-                strcpy(objectcode, mnemonic); 
-                strcat(objectcode, add); 
-            } else { 
-                strcpy(objectcode, mnemonic); 
-                strcat(objectcode, "0000"); 
-            } 
-        } else if (strcmp(opcode, "BYTE") == 0) { 
-            if (operand[0] == 'C') { 
-                strcpy(objectcode, ""); 
-                for (int i = 2; i < strlen(operand) - 1; i++) { 
-                    char hex[3]; 
-                    sprintf(hex, "%02X", operand[i]); 
-                    strcat(objectcode, hex); 
-                } 
-            } else if (operand[0] == 'X') { 
-                strncpy(objectcode, operand + 2, strlen(operand) - 3); 
-                objectcode[strlen(operand) - 3] = '\0'; 
-            } 
-        } else if (strcmp(opcode, "WORD") == 0) sprintf(objectcode, "%06X", atoi(operand)); 
-        else strcpy(objectcode, "-"); 
-        fprintf(fp2, "%04X\t%s\t%s\t%s\t%s\n", locctr, label, opcode, operand, objectcode); 
-        if (strcmp(objectcode, "-") != 0) { 
-            strcat(textRecord, objectcode); 
-            textLen += strlen(objectcode) / 2; 
-        } 
-        if (fscanf(fp1, "%d%s%s%s", &locctr, label, opcode, operand) == EOF) break; 
-    } 
-    fprintf(fp2, "\t%s\t%s\n", opcode, operand); 
-    progLen = locctr - startAddr; 
-    fprintf(fp2, "\nH^%s^%06X^%06X\n", label, startAddr, progLen); 
-    fprintf(fp2, "T^%06X^%02X^%s\n", textStart, textLen, textRecord); 
-    fprintf(fp2, "E^%06X\n", startAddr); 
-    fclose(fp1); 
-    fclose(fp2); 
-    fclose(fp3); 
-    fclose(fp4); 
-    printf("Pass 2 Assembler completed with H T E records. Output written to twoout.txt\n"); 
-    return 0;
+    
+  
+     
+  fprintf(objcode,"\nE^00%d",startaddress);
+  printf("writing complete\n\n");
+  fclose(intermediate);
+  fclose(objcode);
+  fclose(output);  
+  
 }
